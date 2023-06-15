87025ba59b8f8786d59e927480ed93c9e0693028
Agustin <agustinsurila@gmail.com>
2023-06-15 19:11:05 -0300

    Creado un Serializer para Hacer un Circular reference
    El circular Reference nos permite relacionar cada entidad, con la entidad que lo esta relacionando, y devolver la información de esa entidad

--- src/Serializer/CircularReferenceHandler.php
+++ src/Serializer/CircularReferenceHandler.php
@@ -1,2 +1,36 @@
 <?php
 
+namespace App\Serializer;
+
+use App\Entity\Note;
+use App\Entity\Task;
+use App\Entity\TaskList;
+use Symfony\Component\Routing\RouterInterface;
+
+class CircularReferenceHandler
+{
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function __invoke($object)
+    {
+        switch ($object){
+            case $object instanceof TaskList:
+                return $this->router->generate("get_list", ["id" => $object->getId()]);
+
+            case $object instanceof Task:
+                return $this->router->generate("get_task", ["id" => $object->getId()]);
+
+            case $object instanceof Note:
+                return $this->router->generate("get_note", ["id" => $object->getId()]);
+        }
+        return $object->getId();
+    }
+}
